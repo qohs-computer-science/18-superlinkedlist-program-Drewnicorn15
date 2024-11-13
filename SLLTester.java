01dffeaefd67739de1199201f8850084758b5576
Drewnicorn15 <67112695+Drewnicorn15@users.noreply.github.com>
2024-11-13 15:04:52 +0000

    +submit

--- SLLTester.java
+++ SLLTester.java
@@ -1,5 +1,37 @@
 class SLLTester {
   public static void main(String[] args) {
-
+    SuperLinkedList SSL1 = new SuperLinkedList();
+    SSL1.add("a");
+    SSL1.add("b");
+    SSL1.add("c");
+    SSL1.add("b");
+    SSL1.add("a");
+    System.out.println("this is list 1:");
+    System.out.println(SSL1);
+    SuperLinkedList SSL2 = new SuperLinkedList();
+    SSL2.add("a");
+    SSL2.add("b");
+    SSL2.add("c");
+    SSL2.add("d");
+    SSL2.add("e");
+    System.out.println("this is list 2:");
+    System.out.println(SSL2);
+    SuperLinkedList SSL3 = new SuperLinkedList();
+    SSL3.add("A");
+    SSL3.add("E");
+    SSL3.add("B");
+    SSL3.add("F");
+    SSL3.add("X");
+    System.out.println("this is list 3:");
+    System.out.println(SSL3);
+    System.out.println("it is " + SSL3.removeConsonants() +" that consonants were removed from list 3. it now looks like: ");
+    System.out.println(SSL3);
+    System.out.println("it is " + SSL2.removeVowels() +" that vowels were removed from list 2. it now looks like: ");
+    System.out.println(SSL2);
+    System.out.println("it is " + SSL3.removeConsonants() +" that consonants were removed from list 3. it now looks like: ");
+    System.out.println(SSL3);
+    System.out.println("List 1 concatenated: "+ SSL1.concatenateStrings());
+    SSL3.add("O");
+    System.out.println("List 2 and 3 mixed: "+ SSL2.mix(SSL3));
   } // end main
 } // end class
--- SuperLinkedList.java
+++ SuperLinkedList.java
@@ -1,31 +1,73 @@
 import java.util.LinkedList;
-import java.util.Iterator;
+import java.util.ListIterator;
 
 public class SuperLinkedList extends LinkedList<String> {
-    public boolean removeVowels() {
-        return false;
-    }
+  public boolean removeVowels() {
+    boolean changed = false;
+    ListIterator<String> list = this.listIterator();
+    while (list.hasNext()) {
+      String next = list.next().toLowerCase();
+      if ("eioua".contains(next)) {
+        changed = true;
+        list.remove();
+      } // end if
+    } // end while
+    return changed;
+  }// end remove vowels
 
-    public boolean removeConsonants() {
-        return false;
-    }
+  public boolean removeConsonants() {
+    boolean changed = false;
+    ListIterator<String> list = this.listIterator();
+    while (list.hasNext()) {
+      String next = list.next().toLowerCase();
+      if ("qwrtypsdfghjklzxcvbnm".contains(next)) {
+        changed = true;
+        list.remove();
+      } // end if
+    } // end while
+    return changed;
+  }// end removeConsonants
 
-    public LinkedList<String> removeDuplicates() {
-        return null;
-    }
+  public LinkedList<String> concatenateStrings() {
+    LinkedList<String> concat = new LinkedList<String>();
+    ListIterator<String> list = this.listIterator();
+    String current = "";
+    while (list.hasNext()) {
+      current += list.next();
+      concat.add(current);
+    } // end while
+    return concat;
+  }// end concatenateStrings
 
-    public LinkedList<String> concatenateStrings() {
+  public LinkedList<String> mix(LinkedList<String> list2) {
+    ListIterator<String> list = this.listIterator();
+    ListIterator<String> list2iter = list2.listIterator();
+    LinkedList<String> third = new LinkedList<String>();
+    while (list.hasNext()) {
+      third.add(list.next());
+      third.add(list2iter.next());
+    } // end while
+    return third;
+  }// end mix
 
-        return null;
-    }
+  public LinkedList<String> removeDuplicates() {
+    LinkedList<String> dupes = new LinkedList<String>();
+    ListIterator<String> list = this.listIterator();
+    while (list.hasNext()) {
+      String next = list.next();
+      if (this.toString().lastIndexOf(next) != this.toString().indexOf(next)) {
+        dupes.add(next);
+        list.remove();
+      } // end if
+    } // end while
+    return dupes;
+  }// end removeDuplicates
 
-    public LinkedList<String> mix(LinkedList<String> list2) {
-
-        return null;
-    }
-
-    public String toString() {
-
-        return "";
-    }
-} // end SuperLinkedList
+  public String toString() {
+    String out = "";
+    ListIterator<String> list = this.listIterator();
+    while (list.hasNext())
+      out += list.next() + ", ";
+    return out;
+  }
+}// end class
